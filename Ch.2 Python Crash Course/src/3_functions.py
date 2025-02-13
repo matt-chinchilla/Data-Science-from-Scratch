646ca4760b3af650c8e201f2f4f95dc3a3200a8f
matt-chinchilla <70705923+matt-chinchilla@users.noreply.github.com>
2025-02-13 17:12:15 -0500

    update to code for me to pull on laptop

---
+++ Ch.2 Python Crash Course/src/3_functions.py
@@ -0,0 +1,8 @@
+#3a) Functions are a rule for:
+    # Taking 0 or more inputs and returning a corresponding output
+    # Ex:
+def double(x):
+    """ This is where you put an optional docstring that explains what the
+        function does. For example, the function multiplies its imput by 2
+    """
+    return x * 2
